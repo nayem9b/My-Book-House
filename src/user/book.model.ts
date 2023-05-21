41f58dcd93b5d3ce6a5c6efae4e0c858b417fc08
nayem9b <razwanniam1@gmail.com>
2023-05-22 01:25:13 +0600

    model and schema created

---
+++ src/user/book.model.ts
@@ -0,0 +1,69 @@
+import { Schema, model } from "mongoose";
+import { IBooks } from "./book.interface";
+
+const bookSchema = new Schema<IBooks>({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: [String],
+    required: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+  },
+  publicationYear: {
+    type: Number,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Schema.Types.Mixed,
+    required: true,
+  },
+  publisher: {
+    name: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+  },
+  reviews: {
+    type: [
+      {
+        user: {
+          type: String,
+          required: true,
+        },
+        comment: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    required: true,
+  },
+});
+
+const Books = model<IBooks>("Books", bookSchema);
+export default Books;
+
+//     title: string;
+//   author: string[];
+//   genre: string;
+//   publicationYear: number;
+//   rating: number;
+//   price: string | number;
+//   publisher: { name: string; location: string };
+//   reviews: [
+//     { user: string; comment: string },
+//     { user: string; comment: string }
+//   ];
